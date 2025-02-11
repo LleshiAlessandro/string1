46ecf885f13c5bea29452687111f4397436715c8
lleshi_alessandro <lleshi.alessandro@franchettisalviani.net>
2025-02-11 13:32:23 +0100

    prima versione

--- string1/Program.cs
+++ string1/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello!");
+﻿int decimale(string v)
+{
+    int[] m = new int[v.Length];
+    int somma = 0;
+
+    for (int i = 0; i < v.Length; i++)
+    {
+        m[i] = v[i] - '0';
+        somma = somma * 2 + m[i];
+    }
+
+    return somma;
+}
+
+Console.WriteLine("Dimmi il numero binario di massimo 8 cifre");
+string v = Console.ReadLine();
+
+int j = decimale(v);
+Console.WriteLine("Il numero decimale è: " + j);
