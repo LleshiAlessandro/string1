7e7b7e07e9b26feed6bd005093cf76ceb6ffbb60
lleshi_alessandro <lleshi.alessandro@franchettisalviani.net>
2025-02-12 09:19:35 +0100

    seconda e terza versione
    secondavers

--- string1/Program.cs
+++ string1/Program.cs
@@ -12,8 +12,30 @@
     return somma;
 }
 
-Console.WriteLine("Dimmi il numero binario di massimo 8 cifre");
+
+Console.WriteLine("Dimmi il numero binario di massimo 8 cifre ");
 string v = Console.ReadLine();
 
+Console.WriteLine("Dimmi la frase che vuoi ");
+string frase = Console.ReadLine();
+string[] frase1 = frase.Split(" ");
+string frase2 = frase.Substring(0, frase.Length/2);
+string frase3 = frase.Substring(frase.Length/2);
+string risultato = "";
+//es 1
 int j = decimale(v);
 Console.WriteLine("Il numero decimale è: " + j);
+//es 2
+for (int i = 0; i < frase1.Length; i++)
+{
+    if (frase1[i] != "")
+    {
+        risultato += frase1[i] + " ";
+    }
+}
+
+Console.Write(risultato);
+Console.WriteLine(" ");
+//es 3
+Console.WriteLine("la pirma metà è: " + frase2);
+Console.WriteLine("la sedonda metà è: " + frase3);
